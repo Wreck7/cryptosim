b348fe959c73f1b3380fbddd6a034b6ddf533742
Wreck7 <vishwagovula07@gmail.com>
2025-06-27 20:09:54 +0530

    done user profile dashboard!

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
 from backend.routes.auth import router as auth_router
 from backend.routes.dashboard import router as dashboard_router
+from backend.routes.profile import router as profile_router
+
 
 app = FastAPI(title="Crypto Dashboard API")
 
 
 
 app.include_router(auth_router)
+app.include_router(profile_router)
 app.include_router(dashboard_router)
 
